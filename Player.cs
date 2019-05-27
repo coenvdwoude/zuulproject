efd6f3ecd1d77105977e6bee2a0ab021229116bd
Coen van der Woude <coenvdwoude@hotmail.nl>
2019-05-27 09:52:35 +0200

    init commit

---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZuulCS
+{
+    class Player
+    {
+        private Room currentRoom;
+
+        public Player()
+        {
+            
+
+        }
+
+        public void setCurrentRoom(Room input)
+        {
+            currentRoom = input;
+        }
+       
+        public Room getCurrentRoom()
+        {
+            return currentRoom;
+
+        }
+    }
+}
